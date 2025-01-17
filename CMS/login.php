33467d39bcb82c21f89b6ad07ed3242ed9d4e7cf
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-01-17 11:05:46 -0600

    added login styling and function.

--- CMS/login.php
+++ CMS/login.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Drizzled Obsessions Admin</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="CSS/style.css">
+<title>Drizzled Obsessions Admin</title>
 </head>
-<body>
-    <div class="container">
-        <h1 class="login-title">Login</h1>
-        <h2>Please enter your username and password to login.</h2>
-        <div class="login-form">
-            <form action="#">
-                <input type="text" name="username" placeholder="Username">
-                <input type="password" name="password" placeholder="Password">
-                <button type="submit" name="submit">Login</button>
-            </form>
+    <body>
+        <div class="container">
+            <h1 class="login-title">Login</h1>
+                <h2 class="login-subtitle">Please enter your username and password to login.</h2>
+                <div class="login-form">
+                    <form action="#">
+                        <input type="text" name="username" placeholder="Username">
+                        <input type="password" name="password" placeholder="Password">
+                        <button type="submit" name="submit">Login</button>
+                    </form>
+                </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
