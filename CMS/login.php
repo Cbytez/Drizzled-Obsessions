e66887c5e989c2fe6545546ffa0e10efc233cb32
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-01-17 09:58:50 -0600

    added styling and html to login page.

--- CMS/login.php
+++ CMS/login.php
@@ -3,12 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Drizzled Obsessions Admin</title>
 </head>
 <body>
     <div class="container">
-        <h1>Login</h1>
-        <p>Please enter your username and password to login.</p>
+        <h1 class="login-title">Login</h1>
+        <h2>Please enter your username and password to login.</h2>
+        <div class="login-form">
+            <form action="includes/login.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <button type="submit" name="submit">Login</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
