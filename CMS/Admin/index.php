61f6341565497faee9e5f8501cf16c3c2a4ec226
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-01-23 01:20:45 -0600

    added logout page.

--- CMS/Admin/index.php
+++ CMS/Admin/index.php
@@ -4,4 +4,12 @@
 <h1>Hello Admin</h1>
 </div>
 
+<button class="logout_button" onclick="logout()">Logout</button>
+
 <?php include "includes/admin_footer.php"; ?>
+
+<script type="text/javascript">
+function logout(){
+    window.location.href = "../logout.php";
+}
+</script>
