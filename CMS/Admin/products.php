40593459b1a711269b6d36fd238637f3c6d58226
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-02-03 22:22:04 -0600

    added products page.

---
+++ CMS/Admin/products.php
@@ -0,0 +1,71 @@
+<?php include "Includes/admin_header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/584507be96.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <div class="container_admin">
+        <h1 class="admin_header">Drizzled Obsessions Products</h1>        
+
+        <div class="admin_navigation">
+            <ul>
+                <li><a href="index.php"><i class="fa-solid fa-gauge"></i> Dashboard</a></li>
+                <div class="navbar">
+                    <div class="dropdown">
+                        <button class="dropbtn">Products<i class="fa fa-caret-down"></i></button>
+                        <div class="dropdown-content">
+                            <a href="products.php">View All Products</a>
+                            <a href="add_product.php">Add New Product</a>
+                            <a href="edit_product.php">Edit Product</a>
+                            <a href="delete_product.php">Delete Product</a>
+                        </div>
+                    </div>
+                <li><a href="orders.php"><i class="fa-solid fa-cart-shopping"></i> Orders</a></li>
+                <li><a href="customers.php"><i class="fa-solid fa-users"></i> Customers</a></li>
+                <li><a href="sales.php"><i class="fa-solid fa-chart-line"></i> Sales</a></li>
+                <li><a href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
+                
+            </ul>
+        </div>
+
+        <h1>Products</h1>
+        
+        <!-- Content -->
+
+        <?php
+            if(isset($_GET['source'])){
+                $source = escape($_GET['source']);
+            } else {
+                $source = '';
+            }
+
+            switch($source){
+                case 'add_product':
+                    include "includes/add_product.php";
+                    break;
+                    case '100':
+                        echo "great";
+                        break;
+                case 'edit_product':
+                    include "includes/edit_product.php";
+                    break;
+                case 'delete_product':
+                    include "includes/delete_product.php";
+                    break;
+                default:
+                    include "includes/view_all_products.php";
+                    break;
+            }
+        ?>
+    </div>
+</body>
+</html>
+
+
+
+
