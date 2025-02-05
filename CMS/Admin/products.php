c2833fd7fa0eb57c6a1b22d8664f7954b2dd8f31
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-02-04 23:19:48 -0600

    edited navigation functionality and styling for products page.

--- CMS/Admin/products.php
+++ CMS/Admin/products.php
@@ -15,7 +15,14 @@
         <div class="admin_navigation">
             <ul>
                 <li><a href="index.php"><i class="fa-solid fa-gauge"></i> Dashboard</a></li>
-                <li><a href="products.php"><i class="fa-solid fa-box"></i> Products</a></li>
+                <li class="dropdown"><a class="active" href="products.php"><i class="fa-solid fa-box"></i> Products<i class="fa-solid fa-caret-down"></i></a>
+                    <div class="dropdown-content">
+                        <a href="add_product">Add Product</a>
+                        <a href="edit_product">Edit Product</a>
+                        <a href="delete_product">Delete Product</a>
+                    </div>
+                </li>
+
                 <li><a href="orders.php"><i class="fa-solid fa-cart-shopping"></i> Orders</a></li>
                 <li><a href="customers.php"><i class="fa-solid fa-users"></i> Customers</a></li>
                 <li><a href="sales.php"><i class="fa-solid fa-chart-line"></i> Sales</a></li>
@@ -24,7 +31,7 @@
             </ul>
         </div>
 
-        <h1>Products</h1>
+        
         
         <!-- Content -->
 
