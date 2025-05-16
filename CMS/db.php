914428092851e309eb9797f07b3a8fc703cf77c1
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-05-15 22:03:25 -0500

    edited db file for better and accurate connectivity.

--- CMS/db.php
+++ CMS/db.php
@@ -1,28 +1,17 @@
 <?php 
 
-// echo "Hello World! ";
-ob_start();
-	// $db = mysqli_connect('shareddb-z.hosting.stackcp.net', 'CMSAPP-313635097f', '2azoaez7ah', 'CMSAPP-313635097f');
+$host = "localhost";
+$user = "Fatality";
+$pass = "Yennefer0974";
+$name = "Drizzled_Obsessions";
 
-//additional way of connecting to the database that we could use in the future below.
+$dbs = mysqli_connect($host, $user, $pass, $name);
 
-$db['db_host'] = "localhost";
-$db['db_user'] = "fatality";
-$db['db_pass'] = "Yennefer0974";
-$db['db_name'] = "Drizzled_Obsessions";
-
-foreach ($db as $key => $value) {
-	define(strtoupper($key), $value);
+if ($dbs) {
+	echo "We are connected Fool!";
+}else{
+	echo "Not Connected!";
 }
-
-// $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// 	if ($db) {
-// 		echo "We are connected!";
-// 	}else{
-// 		echo "Not Connected!";
-// 	}
-
 
  ?>
 <!-- The php.ini file needs to be updated to allow the rule to show errors = On  so that we can see the errors in the browser .-->
